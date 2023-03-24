e6e766bbca34a5bcf5659d4542ddf546fc090a48
steven <steven@StefanoPugg>
2023-03-24 20:46:08 +0100

    Added exceptions/MalformedMapException.java

---
+++ src/main/java/com/onlinerestaurant/exceptions/MalformedMapException.java
@@ -0,0 +1,16 @@
+package com.onlinerestaurant.exceptions;
+
+/**
+ * Thrown when a Map doesn't contain all the required values
+ */
+public class MalformedMapException extends Exception {
+
+    public MalformedMapException(){
+        super();
+    }
+
+    public MalformedMapException(String message){
+        super(message);
+    }
+    
+}
