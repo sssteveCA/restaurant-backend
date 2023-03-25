c443b83a037d081148a66ab6517601bf4a3ad601
steven <steven@StefanoPugg>
2023-03-25 14:15:51 +0100

    Added spring-data-jpa and mysql-connector-j dependencies

--- src/main/java/com/onlinerestaurant/restaurant/database/models/Dish.java
+++ src/main/java/com/onlinerestaurant/restaurant/database/models/Dish.java
@@ -7,7 +7,7 @@ import com.onlinerestaurant.exceptions.MalformedMapException;
 import com.onlinerestaurant.restaurant.database.Model;
 import com.onlinerestaurant.restaurant.interfaces.Constants;
 
-public class Dish extends Model {
+public class Dish {
 
     private byte id;
     private byte restaurant_id;
@@ -15,66 +15,12 @@ public class Dish extends Model {
     private List<String> ingredients;
     private float price;
 
-    public static final byte ERR_INVALID_DATA = 1;
-
-    private static final String ERR_INVALID_DATA_MSG = Constants.ERR_INVALID_DATA;
-
-    public Dish(Map<String, Object> params) throws MalformedMapException {
-        super(params);
-    }
-
     public byte getId() { return id; }
     public byte getRestaurant_id() { return restaurant_id; }
     public String getName() { return name; }
     public List<String> getIngredients() { return ingredients; }
     public float getPrice() { return price; }
 
-    @Override
-    public String getError() {
-        // TODO Auto-generated method stub
-        switch(this.errno){
-            case ERR_INVALID_DATA:
-                this.error = ERR_INVALID_DATA_MSG;
-                break;
-            default:
-                this.error = null;
-                break;
-        }
-        return this.error;
-    }
+  
 
-    @Override
-    public boolean get(String query){
-        this.errno = 0;
-        super.get(query);
-        return true;
-    }
-
-    @Override
-    public boolean insert(Map<String, Object> params){
-        this.errno = 0;
-        super.insert(params);
-        return true;
-    }
-
-    @Override
-    public boolean update(String query){
-        this.errno = 0;
-        super.update(query);
-        return true;
-    }
-
-    @Override
-    public boolean delete(String query){
-        this.errno = 0;
-        super.delete(query);
-        return true;
-    }
-
-    @Override
-    protected boolean validateData() {
-        // TODO Auto-generated method stub
-        return true;
-    }
-    
 }
