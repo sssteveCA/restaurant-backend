fde13aa8684116d60599920b8f90b5d588ea894d
stepp <stepp@mail.com>
2023-04-12 12:07:50 +0200

    Added checkMap method to TextMail.java

--- src/main/java/com/onlinerestaurant/restaurant/mails/TextMail.java
+++ src/main/java/com/onlinerestaurant/restaurant/mails/TextMail.java
@@ -1,5 +1,7 @@
 package com.onlinerestaurant.restaurant.mails;
 
+import java.util.Map;
+
 import org.springframework.stereotype.Component;
 
 @Component
@@ -13,9 +15,24 @@ public class TextMail{
     
     private String text;
 
+    public TextMail(Map<String, String> data){
+        this.checkMap(data);
+    }
+
     public String getFrom() { return from; }
     public String getTo() { return to; }
     public String getSubject() { return subject; }
     public String getText() { return text; }
+
+    private void checkMap(Map<String, String> data){
+        if(data.containsKey("from") && data.containsKey("to") && data.containsKey("subject") && data.containsKey("text")){
+            this.from = data.get("from");
+            this.to = data.get("to");
+            this.subject = data.get("subject");
+            this.text = data.get("text");
+        }//if(data.containsKey("from") && data.containsKey("to") && data.containsKey("subject") && data.containsKey("text")){
+    }
+
+
     
 }
