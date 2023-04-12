9adc5e436427c35776bf57bc24ba92de5c3a092e
stepp <stepp@mail.com>
2023-04-12 12:12:23 +0200

    Added sendMail method to TextMail

--- src/main/java/com/onlinerestaurant/restaurant/mails/TextMail.java
+++ src/main/java/com/onlinerestaurant/restaurant/mails/TextMail.java
@@ -2,10 +2,18 @@ package com.onlinerestaurant.restaurant.mails;
 
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Component;
+
+import com.onlinerestaurant.restaurant.exceptions.MalformedMapException;
 
 @Component
 public class TextMail{
+
+    @Autowired
+    private JavaMailSender jms;
     
     private String from;
     
@@ -15,8 +23,9 @@ public class TextMail{
     
     private String text;
 
-    public TextMail(Map<String, String> data){
+    public TextMail(Map<String, String> data) throws MalformedMapException{
         this.checkMap(data);
+        this.sendMail();
     }
 
     public String getFrom() { return from; }
@@ -24,13 +33,23 @@ public class TextMail{
     public String getSubject() { return subject; }
     public String getText() { return text; }
 
-    private void checkMap(Map<String, String> data){
+    private void checkMap(Map<String, String> data) throws MalformedMapException{
         if(data.containsKey("from") && data.containsKey("to") && data.containsKey("subject") && data.containsKey("text")){
             this.from = data.get("from");
             this.to = data.get("to");
             this.subject = data.get("subject");
             this.text = data.get("text");
         }//if(data.containsKey("from") && data.containsKey("to") && data.containsKey("subject") && data.containsKey("text")){
+        else throw new MalformedMapException();
+    }
+
+    private void sendMail(){
+        SimpleMailMessage smm = new SimpleMailMessage();
+        smm.setFrom(this.from);
+        smm.setTo(this.to);
+        smm.setSubject(this.subject);
+        smm.setText(this.text);
+        this.jms.send(smm);
     }
 
 
