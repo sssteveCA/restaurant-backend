7796cfa588b4e05ed9b3e638038f54148f064182
stepp <stepp@mail.com>
2023-04-12 19:48:43 +0200

    Added getters and setters to User.java

--- src/main/java/com/onlinerestaurant/restaurant/database/models/User.java
+++ src/main/java/com/onlinerestaurant/restaurant/database/models/User.java
@@ -27,6 +27,14 @@ public class User {
     @Column(nullable = false, length = 64)
     private String password;
 
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
     @Column(nullable = true, name = "verified_at")
     private String verifiedAt;
 
@@ -35,5 +43,21 @@ public class User {
 
     @Column(name = "updated_at", nullable = false, columnDefinition = "DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP ")
     private String updatedAt;
+
+    public Long getId() { return id; }
+    public String getFirstName() { return firstName; }
+    public String getLastName() { return lastName; }
+    public String getEmail() {return email;}
+    public String getVerifiedAt() { return verifiedAt; }
+    public String getUpdatedAt() { return updatedAt; }
+    public String getCreatedAt() { return createdAt; }
+
+    public void setId(Long id) { this.id = id; }
+    public void setFirstName(String firstName) { this.firstName = firstName; }
+    public void setLastName(String lastName) { this.lastName = lastName; }
+    public void setEmail(String email) { this.email = email; }
+    public void setVerifiedAt(String verifiedAt) { this.verifiedAt = verifiedAt; }
+    public void setUpdatedAt(String updatedAt) { this.updatedAt = updatedAt; }
+    public void setCreatedAt(String createdAt) { this.createdAt = createdAt; }
     
 }
