555401810ccbd949e6eb4f7200db950b6347fb24
stepp <stepp@mail.com>
2023-04-12 20:09:19 +0200

    Added requests/Subscribe.java

---
+++ src/main/java/com/onlinerestaurant/restaurant/requests/Subscribe.java
@@ -0,0 +1,10 @@
+package com.onlinerestaurant.restaurant.requests;
+
+public class Subscribe {
+    
+    public String firstName;
+    public String lastName;
+    public String email;
+    public String password;
+    public String confPassword;
+}
