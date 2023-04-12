b7a82527fde9221d6a0267480376738d3a19f85f
stepp <stepp@mail.com>
2023-04-12 20:25:55 +0200

    Added register method to SubscribeController.java

--- src/main/java/com/onlinerestaurant/restaurant/controllers/SubscribeController.java
+++ src/main/java/com/onlinerestaurant/restaurant/controllers/SubscribeController.java
@@ -1,13 +1,57 @@
 package com.onlinerestaurant.restaurant.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.onlinerestaurant.restaurant.database.models.User;
+import com.onlinerestaurant.restaurant.database.repositories.UserRepository;
+import com.onlinerestaurant.restaurant.exceptions.BadRequestException;
+import com.onlinerestaurant.restaurant.interfaces.Constants;
+import com.onlinerestaurant.restaurant.requests.Subscribe;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @RestController
 public class SubscribeController {
+
+    @Autowired
+    private UserRepository userRepository;
     
     @PostMapping("/register")
-    public String register(){
-        return "";
+    public String register(@RequestBody Subscribe subscribe, HttpServletRequest request, HttpServletResponse response) throws JsonProcessingException{
+        ObjectMapper om = new ObjectMapper();
+        ObjectNode on = om.createObjectNode();
+        try{
+            boolean firstNameSet = (subscribe.firstName != null && !subscribe.firstName.equals(""));
+            boolean lastNameSet = (subscribe.lastName != null && !subscribe.lastName.equals(""));
+            boolean emailSet = (subscribe.email != null && !subscribe.email.equals(""));
+            boolean passwordSet = (subscribe.password != null && !subscribe.password.equals(""));
+            boolean confPasswordSet = (subscribe.confPassword != null && !subscribe.confPassword.equals(""));
+            if(firstNameSet && lastNameSet && emailSet && passwordSet && confPasswordSet){
+                if(subscribe.password.equals(subscribe.confPassword)){
+                    User user = new User();
+                    userRepository.save(user);
+                    on.put(Constants.KEY_DONE, true);
+                    on.put(Constants.KEY_MESSAGE, Constants.OK_REGISTRATION);
+                    return om.writerWithDefaultPrettyPrinter().writeValueAsString(on);
+                }//if(subscribe.password.equals(subscribe.confPassword)){
+                throw new BadRequestException(Constants.ERR_PASSWORD_MISMATCH);
+            }//if(firstNameSet && lastNameSet && emailSet && passwordSet && confPasswordSet){
+            throw new BadRequestException(Constants.ERR_MISSING_DATA);
+        }catch(BadRequestException e){
+            on.put(Constants.KEY_DONE, false);
+            on.put(Constants.KEY_MESSAGE, e.getMessage());
+            return om.writerWithDefaultPrettyPrinter().writeValueAsString(on);
+        }catch(Exception e){
+            on.put(Constants.KEY_DONE, false);
+            on.put(Constants.KEY_MESSAGE, Constants.ERR_REQUEST);
+            return om.writerWithDefaultPrettyPrinter().writeValueAsString(on);
+        }
     }
 }
--- src/main/java/com/onlinerestaurant/restaurant/interfaces/Constants.java
+++ src/main/java/com/onlinerestaurant/restaurant/interfaces/Constants.java
@@ -12,6 +12,7 @@ public interface Constants {
     String ERR_INVALID_DATA = "I dati inseriti non sono validi, riprova";
     String ERR_MENU = "Impossibile mostrare i menu del ristorante. "+ADMIN_CONTACT;
     String ERR_MISSING_DATA = "Inserisci i dati richiesti per continuare";
+    String ERR_PASSWORD_MISMATCH = "Le due password inserite non coincidono";
     String ERR_REQUEST = "Errore durante l'esecuzione della richiesta. "+ADMIN_CONTACT;
 
     //Keys
@@ -20,5 +21,6 @@ public interface Constants {
     String KEY_MESSAGE = "message";
 
     //Success
+    String OK_REGISTRATION = "Per completare la registrazione, clicca sul link che ti abbiamo inviato al tuo account di posta";
     String OK_SUPPORT = "Il tuo messaggio è stato inviato. Risponderemo il prima possibile.";
 }
