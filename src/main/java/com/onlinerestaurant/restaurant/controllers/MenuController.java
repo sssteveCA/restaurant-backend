9c8fd2dbe7858fe2e569eb4adfe6071d5f4c0932
stepp <stepp@mail.com>
2023-04-29 12:45:31 +0200

    Added controllers/MenuController.java

---
+++ src/main/java/com/onlinerestaurant/restaurant/controllers/MenuController.java
@@ -0,0 +1,25 @@
+package com.onlinerestaurant.restaurant.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.onlinerestaurant.restaurant.interfaces.Constants;
+
+@RestController
+@RequestMapping
+public class MenuController {
+    
+    @GetMapping("/menu")
+    public String menu() throws JsonProcessingException{
+        ObjectMapper om = new ObjectMapper();
+        ObjectNode on = om.createObjectNode();
+        on.put(Constants.KEY_DONE, true);
+        on.put(Constants.KEY_EMPTY, true);
+        on.put(Constants.KEY_MESSAGE, Constants.EMPTY_MENU);
+        return om.writerWithDefaultPrettyPrinter().writeValueAsString(on);
+    }
+}
